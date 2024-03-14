4804e384606189189159616b5174d307bcdb5ba2
hungnv1312 <nguyenviethung19981312@gmail.com>
2024-03-14 17:57:14 +0700

    Responve UI multi screne & menu mobile

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hi');
+// console.log('Hi');
+
+// Menu click event
+
+const menuBtn = document.querySelector('.header__hamburger');
+const navMobile = document.querySelector('.header__nav');
+
+menuBtn.addEventListener('click', function () {
+  // Your code to be executed when the button is clicked
+  menuBtn.classList.toggle('header__hamburger--actived');
+  navMobile.classList.toggle('header__nav--actived');
+});
